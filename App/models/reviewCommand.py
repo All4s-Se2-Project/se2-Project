a15ba5684c48b73ea6471f7c4a8609a18a970744
kasimt15 <98177976+kasimt15@users.noreply.github.com>
2024-11-27 01:27:08 -0400

    Update reviewCommand.py for a metaconflict error

--- App/models/reviewCommand.py
+++ App/models/reviewCommand.py
@@ -1,18 +1,19 @@
-from sqlalchemy.ext.declarative import declarative_base, AbstractConcreteBase
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from abc import ABC, abstractmethod
-from App.database import db
 
 Base = declarative_base()
 
-class AbstractBase(AbstractConcreteBase, Base):
+class reviewCommand(Base, ABC):
     __abstract__ = True
 
-class reviewCommand(AbstractBase, ABC):
-    __abstract__ = True
     id = Column(Integer, primary_key=True)
     staff_id = Column(Integer, ForeignKey('staff.ID'), nullable=False)
-    student_id = Column(Integer, ForeignKey('staff.ID'), nullable=False)
+    student_id = Column(Integer, ForeignKey('student.ID'), nullable=False)
+
+    staff = relationship('Staff', backref='reviews')
+    student = relationship('Student', backref='reviews')
 
     @abstractmethod
     def execute(self):
