793f2c31c3c3e7987cc392ea2b8a4755100dd3aa
kasimt15 <98177976+kasimt15@users.noreply.github.com>
2024-11-27 01:13:21 -0400

    Update reviewCommand.py to resolve typeerror

--- App/models/reviewCommand.py
+++ App/models/reviewCommand.py
@@ -1,19 +1,18 @@
 from App.database import db
-from abc import ABC, abstractmethod
+from abc import ABCMeta, abstractmethod
 
-class reviewCommand(db.Model, ABC):
-    __abstract__= True
-    id= db.Column(db.Integer, primary_key=True)
-    staff_id= db.Column(db.Integer, db.ForeignKey('staff.ID'), nullable=False)
-    student_id= db.Column(db.Integer, db.ForeignKey('staff.ID'), nullable=False)
-    
-    staff= db.relationship('Staff', backref= "reviews")
-    student= db.relationship("Student", backref="reviews")
+class reviewCommand(db.Model, metaclass=ABCMeta):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    staff_id = db.Column(db.Integer, db.ForeignKey('staff.ID'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('staff.ID'), nullable=False)
+
+    staff = db.relationship('Staff', backref="reviews")
+    student = db.relationship("Student", backref="reviews")
     
     @abstractmethod
     def execute(self):
-        #implement in subclasses
         pass
     
-    def logChange():
+    def logChange(self):
         pass
